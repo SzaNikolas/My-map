369e2427d1f795ac4e57de83183a722b158f8c23
SzaNikolas <sza.nikolas@gmail.com>
2021-03-31 19:37:32 +0200

    Update test_32x32_7.tsx

--- test_32x32_7.tsx
+++ test_32x32_7.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="test_32x32_7" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../../Desktop/map/Planks/test_32x32_7.png" width="32" height="32"/>
+ <image source="../../../Desktop/Wooden tecture/Planks/test_32x32_7.png" width="32" height="32"/>
 </tileset>
