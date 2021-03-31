f5bd19394663920d1d1d00ac0ef464b0e478988b
SzaNikolas <sza.nikolas@gmail.com>
2021-03-31 19:37:18 +0200

    Update 6.tsx

--- 6.tsx
+++ 6.tsx
@@ -1,5 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="6" tilewidth="32" tileheight="32" tilecount="5625" columns="75">
- <grid orientation="orthogonal" width="8" height="8"/>
- <image source="../../../Desktop/map/6.jpg" width="2400" height="2400"/>
+ <image source="../../../Desktop/Wooden tecture/6.jpg" width="2400" height="2400"/>
 </tileset>
