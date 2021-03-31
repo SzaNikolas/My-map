23d4e7a0dd9d2fb05655131dd98f956ec493eb3c
SzaNikolas <sza.nikolas@gmail.com>
2021-03-31 19:37:20 +0200

    Create Bricks_002.tsx

---
+++ Bricks_002.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Bricks_002" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../Downloads/Bricks_002.png" width="512" height="512"/>
+</tileset>
