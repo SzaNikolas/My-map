ce5fb860779bb88964f91e05144a15d61493db89
SzaNikolas <sza.nikolas@gmail.com>
2021-03-31 10:22:58 +0200

    Create qubodup-light_wood.tsx

---
+++ qubodup-light_wood.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="qubodup-light_wood" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../../../Downloads/qubodup-light_wood.png" width="1024" height="1024"/>
+</tileset>
