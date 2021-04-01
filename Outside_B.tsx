e5a285106bd7b00d9a924deb5cb27d50ae6b165f
SzaNikolas <sza.nikolas@gmail.com>
2021-04-01 02:53:09 +0200

    Create Outside_B.tsx

---
+++ Outside_B.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Outside_B" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../../../Desktop/MAP2/Tilesets/Outside_B.png" width="512" height="512"/>
+ <tile id="70">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="174">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="175">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
