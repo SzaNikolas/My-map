decebc94f96e9125292a0930701a4d659bbd14bb
SzaNikolas <73198365+SzaNikolas@users.noreply.github.com>
2021-04-01 00:53:40 +0000

    Deploying to gh-pages from master @ e5a285106bd7b00d9a924deb5cb27d50ae6b165f 🚀

---
+++ Inside_A4.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="Inside_A4" tilewidth="32" tileheight="32" tilecount="240" columns="16">
+ <image source="../../../Desktop/MAP2/Tilesets/Inside_A4.png" width="512" height="480"/>
+ <tile id="52">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
