7da4772d378cbc8aceac077492c41794610b55eb
SzaNikolas <sza.nikolas@gmail.com>
2021-04-01 08:38:28 +0200

    trying to learn

--- Sample1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Sample1" tilewidth="32" tileheight="32" tilecount="448" columns="28">
- <image source="../../../Desktop/map/free/Sample1.png" width="918" height="515"/>
-</tileset>
